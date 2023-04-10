54653757ca279c9b11a6b479abd51f30e1b6aaef
Flávio Eduardo <xflax00@gmail.com>
2023-04-09 22:16:07 -0300

    initial form tests

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const handleForm = () => {
+    let name = document.getElementsById("name")[0].value
+    let email = document.getElementsByName("email")[0].value
+    let pass = document.getElementsByName("pass")[0].value
+
+    console.log(name)
+    console.log(email)
+    console.log(pass)
+}
