fc5b2e41506da2efd3645eb8b7c4976cacafd45c
Ruan Lauro <ruanlaurocardosomonteirogato@gmail.com>
2023-04-20 22:44:41 -0300

    Mudança no aspecto físico

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 // Search input
 let search = document.getElementsByClassName("inp-search")[0]
+let pesq = document.getElementsByClassName("pesq")[0]
 let lupa = document.getElementsByClassName("search-icon")[0]
 let attachment = document.getElementsByClassName("attachment-search")[0]
 let containerSearch = document.getElementsByClassName("search-container")[0]
 
+/*
 // When the search is focused
 search.addEventListener('focus', () => {
     lupa.style.display = 'none'
@@ -19,4 +21,41 @@ search.addEventListener('blur', () => {
 
     attachment.style.display = 'none'
     containerSearch.style = 'padding-bottom: 0rem; background-color: transparent; border-radius: 0px;'
+})
+*/
+
+const meuBotao = document.getElementById("bott");
+let botaoAtivado = false;
+meuBotao.addEventListener("click", function() {
+    console.log(botaoAtivado)
+    if(botaoAtivado == false){
+        pesq.style.display = 'block'
+        search.style.display = 'none'
+        lupa.style.display = 'none'
+        botaoAtivado = true
+    }else if(botaoAtivado == true){
+        pesq.style.display = 'none'
+        search.style.display = 'block'
+        lupa.style.display = 'block'
+        botaoAtivado = false
+    }
+
+  });
+
+  search.addEventListener('focus', () => {
+    lupa.style.display = 'none'
+})
+
+search.addEventListener('blur', () => {
+    if(search.value == '' ){
+        lupa.style.display = 'block'
+    }
+})
+
+pesq.addEventListener('focus', () => {
+    attachment.style.display = 'block'
+})
+
+pesq.addEventListener('blur', () =>{
+    attachment.style.display = 'none'
 })
