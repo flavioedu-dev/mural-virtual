cfd1ab4f6172507f16652f567e045f7f2ec5519c
Flávio Eduardo <xflax00@gmail.com>
2023-04-23 07:52:17 -0300

    pages\home.html: message text field style

--- scripts/script.js
+++ scripts/script.js
@@ -5,24 +5,6 @@ let lupa = document.getElementsByClassName("search-icon")[0]
 let attachment = document.getElementsByClassName("attachment-search")[0]
 let containerSearch = document.getElementsByClassName("search-container")[0]
 
-/*
-// When the search is focused
-search.addEventListener('focus', () => {
-    lupa.style.display = 'none'
-    attachment.style.display = 'block'
-    containerSearch.style = 'padding-bottom: 2rem; background-color: #E0E0E0; border-radius: 5px;'
-})
-
-// When the research loses focus
-search.addEventListener('blur', () => {
-    if(search.value == ''){
-        lupa.style.display = 'block'
-    }
-
-    attachment.style.display = 'none'
-    containerSearch.style = 'padding-bottom: 0rem; background-color: transparent; border-radius: 0px;'
-})
-*/
 
 const meuBotao = document.getElementById("bott");
 let botaoAtivado = false;
@@ -40,9 +22,9 @@ meuBotao.addEventListener("click", function() {
         botaoAtivado = false
     }
 
-  });
+});
 
-  search.addEventListener('focus', () => {
+search.addEventListener('focus', () => {
     lupa.style.display = 'none'
 })
 
@@ -54,8 +36,10 @@ search.addEventListener('blur', () => {
 
 pesq.addEventListener('focus', () => {
     attachment.style.display = 'block'
+    containerSearch.style = "padding-bottom: 2rem; background-color: #E0E0E0; border-radius: 5px;"
 })
 
 pesq.addEventListener('blur', () =>{
     attachment.style.display = 'none'
+    containerSearch.style = "padding-bottom: -2rem;"
 })
