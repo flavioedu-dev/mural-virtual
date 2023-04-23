48a6f261d08989225472699ec0e345c3f4b79532
Flávio Eduardo <xflax00@gmail.com>
2023-04-23 12:01:17 -0300

    scripts\script.js: attachment file

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let pesq = document.getElementsByClassName("pesq")[0]
 let lupa = document.getElementsByClassName("search-icon")[0]
 let attachment = document.getElementsByClassName("attachment-search")[0]
 let containerSearch = document.getElementsByClassName("search-container")[0]
+let file = document.getElementsByClassName("inp-file")[0]
 
 
 const meuBotao = document.getElementById("bott");
@@ -34,14 +35,17 @@ search.addEventListener('blur', () => {
 })
 
 pesq.addEventListener('focus', () => {
-    attachment.style.display = 'block'
-    containerSearch.style = "padding-bottom: 2rem; background-color: #E0E0E0; border-radius: 5px;"
+    attachment.style.opacity = 1
 })
 
 pesq.addEventListener('blur', () =>{
-    attachment.style.display = 'none'
-    containerSearch.style = "padding-bottom: -2rem;"
+    attachment.style.opacity = 0
 })
+
+// Attachment file
+attachment.onclick = () => {
+    file.click()
+}
 
 // Hiding information the exceeds the limit
 let textPost = document.getElementsByClassName("text-post")
