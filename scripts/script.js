e56c076273b3799baa3dce8d6d174c380a2dc09e
Flávio Eduardo <xflax00@gmail.com>
2023-04-24 21:37:55 -0300

    pages\home.html: inserting attachment files

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let pesq = document.getElementsByClassName("pesq")[0]
 let lupa = document.getElementsByClassName("search-icon")[0]
 let attachment = document.getElementsByClassName("attachment-search")[0]
 let containerSearch = document.getElementsByClassName("search-container")[0]
+let attachmentFiles = document.getElementsByClassName("attachment-files")[0]
 let file = document.getElementsByClassName("inp-file")[0]
 
 
@@ -11,15 +12,25 @@ const meuBotao = document.getElementById("bott");
 let botaoAtivado = false;
 meuBotao.addEventListener("click", function() {
     if(botaoAtivado == false){
-        pesq.style.display = 'block'
-        search.style.display = 'none'
-        lupa.style.display = 'none'
-        botaoAtivado = true
+            pesq.style.display = 'block'
+            search.style.display = 'none'
+            lupa.style.display = 'none'
+            botaoAtivado = true
+            attachment.style.display = 'block'
+            containerSearch.style = "background-color: #E0E0E0;"
+
+            if(attachmentFiles.children.length !== 0){
+                attachmentFiles.style.display = 'flex'
+                pesq.style.borderBottom = '10px solid transparent'
+            }
     }else if(botaoAtivado == true){
-        pesq.style.display = 'none'
-        search.style.display = 'block'
-        lupa.style.display = 'block'
-        botaoAtivado = false
+            pesq.style.display = 'none'
+            search.style.display = 'block'
+            lupa.style.display = 'block'
+            botaoAtivado = false
+            attachment.style.display = 'none'
+            attachmentFiles.style.display = 'none'
+            containerSearch.style = "background-color: transparent;"   
     }
 
 });
@@ -34,17 +45,33 @@ search.addEventListener('blur', () => {
     }
 })
 
-pesq.addEventListener('focus', () => {
-    attachment.style.opacity = 1
-})
-
-pesq.addEventListener('blur', () =>{
-    attachment.style.opacity = 0
-})
-
 // Attachment file
 attachment.onclick = () => {
     file.click()
+}
+
+
+file.onchange = () => {
+    if(file){
+        // Show attachment files when exist file
+        if(attachmentFiles.children){
+            attachmentFiles.style.display = 'flex'
+            pesq.style.borderBottom = '10px solid transparent'
+        }
+
+        // Taking item and inserting files
+        const [itemFile] = file.files
+
+        let itemURL = URL.createObjectURL(itemFile)
+
+        let newItem = document.createElement("a")
+        newItem.innerHTML = itemFile.name
+        newItem.setAttribute("href", `${itemURL}`)
+        newItem.setAttribute("target", `_blank`)
+        newItem.setAttribute("class", `file-item`)
+
+        attachmentFiles.appendChild(newItem)
+    }
 }
 
 // Hiding information the exceeds the limit
