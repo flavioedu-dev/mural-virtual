b64425892d33e082606913ac0d5b1248615f47fb
Flávio Eduardo <xflax00@gmail.com>
2023-05-09 23:22:21 -0300

    feat: added feature to register users in the database

---
+++ scripts/api.js
@@ -0,0 +1,38 @@
+const url = "http://localhost:8000"
+
+const register = async () => {
+  const nome = document.getElementById("user").value
+  const email = document.getElementById("email").value
+  const senha = document.getElementById("pass").value
+  const confirmaSenha = document.getElementById("confirm-pass").value
+
+  if(senha !== confirmaSenha){
+    console.log("As senhas precisam ser iguais!")
+    return
+  }
+
+  const user = {
+    nome,
+    email,
+    senha
+  }
+
+  const res = await fetch(`${url}/users`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(user)
+  }).then(res => res).catch(err => console.log(err))
+
+  const data = await res.json()
+  console.log(data)
+
+  // Checking if the user was successfully created and redirecting
+  if(res.status === 201){
+    console.log("Usuário criado com sucesso!")
+    setTimeout(() => {
+      window.location.replace("../pages/login.html")
+    }, 1000)
+  }
+}
