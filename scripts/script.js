1c63112e48fc13ec16d478bac638ca4b6dafaee2
Flávio Eduardo <xflax00@gmail.com>
2023-05-29 22:22:07 -0300

    feat: added post details page and redirect func

--- scripts/script.js
+++ scripts/script.js
@@ -38,10 +38,11 @@ let postsContainer = document.getElementsByClassName("posts-container")[0];
 const postsContainerArray = [...postsContainer.children]
 console.log(postsContainerArray)
 console.log(postsContainer.children)
-postsContainerArray.forEach((post) => {
+postsContainerArray.forEach((post, i) => {
     post.addEventListener("click", () => {
           console.log(post)
           post.setAttribute("class", "post post-selected")
+          window.location.replace(`./post.html?id=${i}`)
         });
 })
 
